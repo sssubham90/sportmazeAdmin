4a3d8f3072940532302ecd1d15fd260eafc35c3c
sssubham90 <yoyosssubham@gmail.com>
2018-03-09 14:55:16 +0530

    Done

--- app/src/main/java/com/devil/sportmazeadmin/UserWatchlistActivity.java
+++ app/src/main/java/com/devil/sportmazeadmin/UserWatchlistActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,13 +20,15 @@ public class UserWatchlistActivity extends AppCompatActivity {
     ArrayList<Date> dates;
     private DatabaseReference mRef;
     private WatchHistoryAdapter mAdapter;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_watchlist);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        final String name = getIntent().getStringExtra("name");
+        name = getIntent().getStringExtra("name");
+        ((TextView)findViewById(R.id.name)).setText(String.format("%s%s", ((TextView) findViewById(R.id.name)).getText(), name));
         dates = new ArrayList<>();
         mAdapter = new WatchHistoryAdapter(this,dates);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
@@ -36,7 +39,10 @@ public class UserWatchlistActivity extends AppCompatActivity {
         mRef.child(name).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("duration")!=null)
+                ((TextView)findViewById(R.id.duration)).setText(String.format("%s%s", ((TextView) findViewById(R.id.duration)).getText(),mFormat(dataSnapshot.child("Total Duration").getValue().toString())));
                 for(DataSnapshot childSnapshot:dataSnapshot.getChildren()){
+                    if(childSnapshot.getKey().equals("Total Duration")) continue;
                     dates.add(new Date(name,childSnapshot.getKey()));
                     mAdapter.notifyDataSetChanged();
                 }
@@ -46,5 +52,12 @@ public class UserWatchlistActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private String mFormat(String s) {
+    String string;
+    long durMin = Long.parseLong(s);
+    string = (durMin/60)+"hours "+(durMin-((durMin/60)*60))+"minutes";
+    return string;
     }
 }
